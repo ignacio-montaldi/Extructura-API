09fe8d968a9eb51526cc6d3ee8da81e19c9565e2
Ignacio Montaldi <javier.montaldi@gmail.com>
2024-05-31 15:15:04 -0300

    test estaba mal hecho, los resultados eran mejores

--- raw_scripts/script_runner.py
+++ raw_scripts/script_runner.py
@@ -20,12 +20,23 @@ from raw_scripts.raw_script_v3_function import main_code
 
 start_time = time.time()
 
-i = 16
-while i <= 16:
-
-    main_code(f"{i:02}", "testing_scan", Image_type.scan)
-    print("Factura " + f"{i:02}" + " scan: ok")
-
+i = 1
+while i <= 30:
+    try:
+        main_code(f"{i:02}", "testing", Image_type.pdf)
+        print("Factura " + f"{i:02}" + " pdf: ok")
+    except:
+        print("Factura " + f"{i:02}" + " pdf: bad")
+    try:
+        main_code(f"{i:02}", "testing_photo", Image_type.photo)
+        print("Factura " + f"{i:02}" + " photo: ok")
+    except:
+        print("Factura " + f"{i:02}" + " photo: bad")
+    try:
+        main_code(f"{i:02}", "testing_scan", Image_type.scan)
+        print("Factura " + f"{i:02}" + " scan: ok")
+    except:
+        print("Factura " + f"{i:02}" + " scan: bad")
     i = i + 1
 
 
